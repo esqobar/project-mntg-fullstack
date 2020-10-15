fe6c596f1d839881f4f43061c516b2deb49b73d2
collins juma <collecollins@gmail.com>
2020-10-15 12:56:43 +0300

    first commit

---
+++ project-mngt-backend/src/main/java/com/esqobar/projectmngtbackend/ProjectMngtBackendApplication.java
@@ -0,0 +1,13 @@
+package com.esqobar.projectmngtbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMngtBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectMngtBackendApplication.class, args);
+    }
+
+}
---
+++ project-mngt-backend/src/test/java/com/esqobar/projectmngtbackend/ProjectMngtBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esqobar.projectmngtbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectMngtBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
